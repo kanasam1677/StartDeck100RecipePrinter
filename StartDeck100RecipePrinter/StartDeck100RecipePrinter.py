7244945b0bc4a289bbd936728d185a3bc8f3c6a8
kanasam1677 <98017291+kanasam1677@users.noreply.github.com>
2022-05-07 19:10:20 +0900

    カードリスト画像の取得を実装

--- StartDeck100RecipePrinter/StartDeck100RecipePrinter.py
+++ StartDeck100RecipePrinter/StartDeck100RecipePrinter.py
@@ -1,8 +1,11 @@
 import sys
+import os
 import requests
+from urllib import parse
 from requests.exceptions import Timeout
 from bs4 import BeautifulSoup
 
+tmpImageFolder="./tmpimage/"
 
 def GetPageContents():
     targetPageURL="https://www.pokemon-card.com/ex/si/index.html"
@@ -20,10 +23,35 @@ def GetPageContents():
 
     return souped
 
+pictureCacheList = []
+def GetPicture(relativePath:str):
+    targetPageBaseURL="https://www.pokemon-card.com/ex/si/"
+    fullURL=parse.urljoin(targetPageBaseURL, relativePath)
+    filename = os.path.split(fullURL)[1]
+    filePath = tmpImageFolder + filename
+    if fullURL not in pictureCacheList:
+        
+        try:
+            r= requests.get(fullURL, timeout=(3.0,7.5))
+        except Timeout:
+            sys.stderr.write("サーバに接続できませんでした\n")
+            exit(1)
+
+        if r.status_code != 200 :
+            sys.stderr.write("画像を取得できませんでした\n")
+            exit(1)
+        with open(filePath,"wb") as f:
+            f.write(r.content)
+        print("サーバより%sを取得しました"%filename)
+        pictureCacheList.append(fullURL)
+    else:
+        print("%sの取得をスキップします"%filename)
+    return filePath
+
+
 def File2DeckNum(filepath:str):
-    f=open(filepath, "r")
-    text = f.read()
-    f.close()
+    with open(filepath, "r") as f:
+        text = f.read()
     splited=text.split()
     if len([i for i in splited if not i.isdigit()]) != 0:
         sys.stderr.write("デッキ番号ファイルに不正な文字が含まれています\n")
@@ -31,14 +59,15 @@ def File2DeckNum(filepath:str):
     return splited
 
 def GetDeckContents(deckNum, souped:BeautifulSoup):
+    print("デッキ番号%sのデータを取得"%deckNum)
     targetDeckContentsClass="modal-deck-%s" % deckNum
     deckContents=souped.find("div",class_=targetDeckContentsClass)
     title = deckContents.find("h3").text
     desc = deckContents.find("div",class_="lyt-group-content").text.replace(title,"")
     desc = desc.replace(title,"").replace("\n","")
     cardList = deckContents.find("div",class_="lyt-group-image").find("img")
-    print(title)
-    print(desc)
+    cardListFilePath=GetPicture(cardList["src"])
+    
 
 def MakeSheet(filepath:str, souped:BeautifulSoup):
     print("%sのシート作成を開始します" % filepath)
@@ -51,6 +80,9 @@ args=sys.argv
 if len(args) < 2:
     sys.stderr.write("出力するデッキ番号を改行区切りで並べたテキストファイルをドラッグアンドドロップしてください\n")
     exit(1)
+
+if not os.path.exists(tmpImageFolder):
+    os.mkdir(tmpImageFolder)
 
 print("公式サイトより情報を取得しています...")
 pageContents=GetPageContents()
