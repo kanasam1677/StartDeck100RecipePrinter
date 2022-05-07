562e298a1264286e9f81ec7b79a68a190c57a6e7
kanasam1677 <98017291+kanasam1677@users.noreply.github.com>
2022-05-07 16:10:54 +0900

    各ファイルパスを受け取る関数を用意、整理

--- StartDeck100RecipePrinter/StartDeck100RecipePrinter.py
+++ StartDeck100RecipePrinter/StartDeck100RecipePrinter.py
@@ -2,25 +2,37 @@ import sys
 import requests
 from requests.exceptions import Timeout
 
+
+def GetPageContents():
+    targetPageURL="https://www.pokemon-card.com/ex/si/index.html"
+    try:
+        r= requests.get(targetPageURL, timeout=(3.0,7.5))
+    except Timeout:
+        sys.stderr.write("サーバに接続できませんでした\n")
+        exit(1)
+
+    if r.status_code != 200 :
+        sys.stderr.write("ページを取得できませんでした\n")
+        exit(1)
+
+    return r.text
+
+def MakeSheet(filepath:str):
+    print(filepath)
+
+
 args=sys.argv
 if len(args) < 2:
     sys.stderr.write("出力するデッキ番号を改行区切りで並べたテキストファイルをドラッグアンドドロップしてください\n")
     exit(1)
 
+pageContents=GetPageContents()
 
-print(args)
+for filepath in args[1:]:
+    MakeSheet(filepath)
 
 
-targetPageURL="https://www.pokemon-card.com/ex/si/index.html"
 
-try:
-    r= requests.get(targetPageURL, timeout=(3.0,7.5))
-except Timeout:
-    sys.stderr.write("サーバに接続できませんでした\n")
-    exit(1)
 
-if r.status_code != 200 :
-    sys.stderr.write("ページを取得できませんでした\n")
-    exit(1)
 
-print(r.text)
+
