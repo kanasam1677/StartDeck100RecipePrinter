6efcfecd14766558e79febd62796b174da5d55f3
kanasam1677 <98017291+kanasam1677@users.noreply.github.com>
2022-05-07 16:01:18 +0900

    コマンドライン引数の取得

--- StartDeck100RecipePrinter/StartDeck100RecipePrinter.py
+++ StartDeck100RecipePrinter/StartDeck100RecipePrinter.py
@@ -2,16 +2,25 @@ import sys
 import requests
 from requests.exceptions import Timeout
 
+args=sys.argv
+if len(args) < 2:
+    sys.stderr.write("出力するデッキ番号を改行区切りで並べたテキストファイルをドラッグアンドドロップしてください\n")
+    exit(1)
+
+
+print(args)
+
+
 targetPageURL="https://www.pokemon-card.com/ex/si/index.html"
 
 try:
     r= requests.get(targetPageURL, timeout=(3.0,7.5))
 except Timeout:
-    sys.stderr.write("サーバに接続できませんでした")
+    sys.stderr.write("サーバに接続できませんでした\n")
     exit(1)
 
 if r.status_code != 200 :
-    sys.stderr.write("ページを取得できませんでした")
+    sys.stderr.write("ページを取得できませんでした\n")
     exit(1)
 
 print(r.text)
