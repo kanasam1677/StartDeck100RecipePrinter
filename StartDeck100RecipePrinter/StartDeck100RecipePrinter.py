338c425fbb09b6648a39dbfc00f9e58f79ea7914
kanasam1677 <98017291+kanasam1677@users.noreply.github.com>
2022-05-07 15:42:53 +0900

    取得失敗した際の軽いエラー処理

--- StartDeck100RecipePrinter/StartDeck100RecipePrinter.py
+++ StartDeck100RecipePrinter/StartDeck100RecipePrinter.py
@@ -1,10 +1,17 @@
-
+import sys
 import requests
+from requests.exceptions import Timeout
 
 targetPageURL="https://www.pokemon-card.com/ex/si/index.html"
 
-r= requests.get(targetPageURL)
+try:
+    r= requests.get(targetPageURL, timeout=(3.0,7.5))
+except Timeout:
+    sys.stderr.write("サーバに接続できませんでした")
+    exit(1)
 
-print(r.status_code)
+if r.status_code != 200 :
+    sys.stderr.write("ページを取得できませんでした")
+    exit(1)
 
 print(r.text)
