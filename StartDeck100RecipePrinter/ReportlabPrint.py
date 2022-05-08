b2b9c1cb8a82fe282513bed8473ec5ef76b00843
kanasam1677 <98017291+kanasam1677@users.noreply.github.com>
2022-05-08 17:47:18 +0900

    枠の印刷

--- StartDeck100RecipePrinter/ReportlabPrint.py
+++ StartDeck100RecipePrinter/ReportlabPrint.py
@@ -1,6 +1,11 @@
 import dataclasses
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4, portrait
+
+#段組みの参考：https://qiita.com/kokardy/items/92e8f3b65c965e20de34
+from reportlab.platypus import BaseDocTemplate, PageTemplate
+from reportlab.platypus import Paragraph, Image, PageBreak, FrameBreak
+from reportlab.platypus.frames import Frame
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.pagesizes import A4, portrait ,mm
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
@@ -11,15 +16,60 @@ class DeckData:
     description:str
     cardListFilePath:str
 
-def MakePdf(filename:str, data:list):
+def MakeFrames():
+    cutNum = (3,3)
+    cardSize=(62 * mm, 86 * mm) #実際のカードサイズは63x88なので1周り小さく
+    paperSize=portrait(A4)
+    margin = ((paperSize[0] - cardSize[0]*cutNum[0])/2,(paperSize[1] - cardSize[1]*cutNum[1])/2)
+    frames=[]
+    for y in range(cutNum[1]):
+        for x in range(cutNum[0]):
+            frames.append(
+                Frame(margin[0] + cardSize[0]*x,
+                      margin[1] + cardSize[1]*y,
+                      cardSize[0],
+                      cardSize[1],
+                      showBoundary=1
+                      )
+            )
+
+    return frames
+
+
+def DrawDeckData(flowables:list, data:DeckData):
+    style_dict ={
+        "name":"title",
+        "fontName":"GenShinGothicPB",
+        "fontSize":20,
+        "leading":20,
+        "firstLineIndent":20,
+        }
+    style = ParagraphStyle(**style_dict)
+    para=Paragraph(data.deckNum,style)
+    flowables.append(para)
+    para=Paragraph(data.title,style)
+    flowables.append(para)
+    para=Paragraph(data.description,style)
+    flowables.append(para)
+    para=Paragraph(data.cardListFilePath,style)
+    flowables.append(para)
+
+
+def MakePdf(filename:str, dataList:list):
     newPdfName = filename + ".pdf"
-    page = canvas.Canvas(newPdfName, pagesize=portrait(A4))
+    doc = BaseDocTemplate(newPdfName, pagesize=portrait(A4)) #portrait(A4)はA4縦の意。landscape(A4)でA4横
     pdfmetrics.registerFont(TTFont("GenShinGothicPB", "./fonts/GenShinGothic-P-Bold.ttf"))
     pdfmetrics.registerFont(TTFont("GenShinGothicPN", "./fonts/GenShinGothic-P-Normal.ttf"))
-    page.setFont("GenShinGothicPB",20)
-    page.drawCentredString(100,100,data[0].title)
-    page.showPage()
-    page.save()
+    frames = MakeFrames()
+    page_temprate = PageTemplate("frames", frames = frames)
+    doc.addPageTemplates(page_temprate)
+    
+    flowables = []
+    for ind, data in enumerate(dataList):
+        DrawDeckData(flowables, data)
+        flowables.append(FrameBreak())
+
+    doc.multiBuild(flowables)
 
     return newPdfName
 
@@ -27,4 +77,15 @@ def MakePdf(filename:str, data:list):
 if __name__ == "__main__":
     testDeckList=[]
     testDeckList.append(DeckData("11", "test1", "description", "./tmpimage/test.png"))
+    testDeckList.append(DeckData("12", "test1", "description", "./tmpimage/test.png"))
+    testDeckList.append(DeckData("13", "test1", "description", "./tmpimage/test.png"))
+    testDeckList.append(DeckData("21", "test1", "description", "./tmpimage/test.png"))
+    testDeckList.append(DeckData("22", "test1", "description", "./tmpimage/test.png"))
+    testDeckList.append(DeckData("23", "test1", "description", "./tmpimage/test.png"))
+    testDeckList.append(DeckData("31", "test1", "description", "./tmpimage/test.png"))
+    testDeckList.append(DeckData("32", "test1", "description", "./tmpimage/test.png"))
+    testDeckList.append(DeckData("33", "test1", "description", "./tmpimage/test.png"))
+    testDeckList.append(DeckData("41", "test1", "description", "./tmpimage/test.png"))
+    testDeckList.append(DeckData("42", "test1", "description", "./tmpimage/test.png"))
+    testDeckList.append(DeckData("43", "test1", "description", "./tmpimage/test.png"))
     MakePdf("test.pdf", testDeckList)
